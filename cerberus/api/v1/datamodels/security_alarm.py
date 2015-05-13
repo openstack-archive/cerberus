81e069ca0c1e160accaf5bebfd8243d894495def
Romain Ziba <romain.ziba@eurogiciel.fr>
2015-05-13 16:50:00 +0200

    Add samples in datamodels for documentation
    Also correct a spelling error SecurityAlarm in security reports
    
    Change-Id: I350499aec9ff7046f6a4eb2692beb45a6c79109c

--- cerberus/api/v1/datamodels/security_alarm.py
+++ cerberus/api/v1/datamodels/security_alarm.py
@@ -70,6 +70,22 @@ class SecurityAlarmResource(base.Base):
             for key in initial_data:
                 setattr(self, key, initial_data[key])
 
+    @classmethod
+    def sample(cls):
+        sample = cls(initial_data={
+            'id': 1,
+            'plugin_id': '927c8435-f81f-468a-92cb-ebb08ed0fad2',
+            'alarm_id': 'fea4b170-ed46-4a50-8b91-ed1c6876be7d',
+            'timestamp': '2015-03-24 09:50:50.577840',
+            'status': 'new',
+            'severity': 'critical',
+            'project_id': 'e845a1f2004847e4ac14cb1732a2e75f',
+            'component_id': '4b75699f7a9649438932bebdbf9711e0',
+            'summary': 'Several attempts to log failed',
+            'description': 'Apache suffered an attack by brute force.'
+                           ' Thousands of attempts to log failed'})
+        return sample
+
 
 class SecurityAlarmResourceCollection(base.Base):
     """A list of Security alarms."""
